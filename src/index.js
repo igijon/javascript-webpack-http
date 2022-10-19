ad7a07db451baefd73c92e47197010be10a77ae8
Inma Gijón <igijoninf@gmail.com>
2022-10-19 18:36:47 +0200

    Página para mostrar chistes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { obtenerChiste } from "./js/http-provider";
+import { init } from "./js/chistes-page";
 
-
-obtenerChiste().then(console.log);
+init();
--- src/js/chistes-page.js
+++ src/js/chistes-page.js
@@ -1,1 +1,42 @@
+import { obtenerChiste } from "./http-provider";
+
 const body = document.body;
+let btnOtro, olList;  //Estos elementos aún no están creados en el HTML 
+
+const crearChistesHtml = () => {
+    const html = `
+    <h1 class="mt-5">Chistes</h1>
+    <hr>
+    <button class="btn btn-primary">Otro chiste </button>
+    <ol class="mt-2 list-group">
+    </ol>
+    `;
+    const divChistes = document.createElement('div');
+    divChistes.innerHTML = html;
+    body.append(divChistes);
+}
+
+
+const eventos =  () => {
+    olList = document.querySelector('ol');
+    btnOtro = document.querySelector('button');
+    btnOtro.addEventListener('click', async () => {
+        btnOtro.disable = true;
+        dibujarChiste( await obtenerChiste() );
+        btnOtro.disable = false;
+    })
+}
+
+//Chiste {id, value}
+const dibujarChiste = ( chiste ) => {
+    const olItem = document.createElement('li');
+    olItem.innerHTML = `<strong>${ chiste.id }</strong>: ${chiste.value}`;
+    olItem.classList.add('list-group-item');
+    olList.append(olItem);
+}
+
+
+export const init = () => {
+    crearChistesHtml();
+    eventos();
+}
